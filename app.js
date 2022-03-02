a4b4cc43748dc435e21cfe82d7501e9669a824eb
Beau Elliott <belliott1516@gmail.com>
2022-03-02 15:13:54 -0800

    got selector from html and data.js into app.js'

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // import functions
-
+import { people } from './data.js';
+console.log(people, 'people');
 // grab DOM elements
-
+const selectEL = document.querySelector('.select');
+console.log(selectEL);
 // set event listeners 
     // get user input
     // use user input to update state 
